08c4b98e8ffc040c3c6e5e8529c63b8794442031
99embee <125384441+99embee@users.noreply.github.com>
2025-02-10 22:04:08 +0000

    New Application in Python.

---
+++ Dice.py
@@ -0,0 +1,27 @@
+import random
+
+class Dice:
+    def __init__(self, sides):
+        self.sides = sides
+
+    @property
+    def sides(self):
+        return self._sides
+
+    @sides.setter
+    def sides(self, value):
+        if value < 1:
+            raise ValueError("Number of sides must be at least 1")
+        self._sides = value
+
+    def roll(self, times):
+        total = 0
+        rolls = ""
+        for _ in range(times):
+            num = random.randint(1, self.sides)
+            total += num
+            if times > 1:
+                rolls += str(num) + ","
+            else:
+                rolls += str(num)
+        return total, rolls
---
+++ DnD_App.py
@@ -0,0 +1,110 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+from Dice import Dice
+
+class DnDApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("DnD Game")
+
+        self.D4 = Dice(4)
+        self.D6 = Dice(6)
+        self.D8 = Dice(8)
+        self.D10 = Dice(10)
+        self.D20 = Dice(20)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.d4_edit = tk.Entry(self.root)
+        self.d4_edit.pack(pady=10)
+        self.d4_button = tk.Button(self.root, text="Roll D4", command=lambda:self.roll_dice(self.D4, int(self.d4_edit.get())))
+        self.d4_button.pack(pady=10)
+        self.d4_label = tk.Label(self.root, text="")
+        self.d4_label.pack(pady=10)
+
+        self.d6_edit = tk.Entry(self.root)
+        self.d6_edit.pack(pady=10)
+        self.d6_button = tk.Button(self.root, text="Roll D6", command=lambda:self.roll_dice(self.D6, int(self.d6_edit.get() )))
+        self.d6_button.pack(pady=10)
+        self.d6_label = tk.Label(self.root, text="")
+        self.d6_label.pack(pady=10)
+
+        self.d8_edit = tk.Entry(self.root)
+        self.d8_edit.pack(pady=10)
+        self.d8_button = tk.Button(self.root, text="Roll D8", command=lambda:self.roll_dice(self.D8, int(self.d8_edit.get() )))
+        self.d8_button.pack(pady=10)
+        self.d8_label = tk.Label(self.root, text="")
+        self.d8_label.pack(pady=10)
+
+        self.d10_edit = tk.Entry(self.root)
+        self.d10_edit.pack(pady=10)
+        self.d10_button = tk.Button(self.root, text="Roll D10", command=lambda:self.roll_dice(self.D10, int(self.d10_edit.get())) )
+        self.d10_button.pack(pady=10)
+        self.d10_label = tk.Label(self.root, text="")
+        self.d10_label.pack(pady=10)
+
+        self.d20_edit = tk.Entry(self.root)
+        self.d20_edit.pack(pady=10)
+        self.d20_button = tk.Button(self.root, text="Roll D20", command=lambda:self.roll_dice(self.D20,  int(self.d20_edit.get()) ))
+        self.d20_button.pack(pady=10)
+        self.d20_label = tk.Label(self.root, text="")
+        self.d20_label.pack(pady=10)
+
+
+        # self.map_button = tk.Button(self.root, text="Show Map", command=self.show_map)
+        # self.map_button.pack(pady=10)
+        self.character_create_button = tk.Button(self.root, text="Create Character", command=self.create_character)
+        self.character_create_button.pack(pady=10)
+
+    def create_character(self):
+        new_window = tk.Toplevel(self.root)
+        new_window.title("Create Character")
+
+        name_label = tk.Label(new_window, text="Name:")
+        name_label.pack(pady=5)
+        name_entry = tk.Entry(new_window)
+        name_entry.pack(pady=5)
+
+        race_label = tk.Label(new_window, text="Race:")
+        race_label.pack(pady=5)
+        race_entry = tk.Entry(new_window)
+        race_entry.pack(pady=5)
+
+        class_label = tk.Label(new_window, text="Class:")
+        class_label.pack(pady=5)
+        class_entry = tk.Entry(new_window)
+        class_entry.pack(pady=5)
+
+        create_button = tk.Button(new_window, text="Create", command=lambda: self.save_character(new_window, name_entry.get(), race_entry.get(), class_entry.get()))
+        create_button.pack(pady=10)
+
+    def save_character(self, window, name, race, char_class):
+        # Here you can add the logic to save the character
+        print(f"Character Created: {name}, {race}, {char_class}")
+        messagebox.showinfo("Character Created", f"Character Created: {name}, {race}, {char_class}")
+        window.destroy()
+
+
+
+    def roll_dice(self, dice, times):  
+        result, rolls = dice.roll(times)
+        if dice == self.D4:
+            self.d4_label.config(text=f"Result: {rolls} = {result}")
+        elif dice == self.D6:
+            self.d6_label.config(text=f"Result: {rolls} = {result}")
+        elif dice == self.D8:
+            self.d8_label.config(text=f"Result: {rolls} = {result}")
+        elif dice == self.D10:
+            self.d10_label.config(text=f"Result: {rolls} = {result}")
+        elif dice == self.D20:
+            self.d20_label.config(text=f"Result: {rolls} = {result}")
+
+    def show_map(self):
+        messagebox.showinfo("Map", "Interactive map will be displayed here.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DnDApp(root)
+    root.mainloop()
