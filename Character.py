4ea16bb55020bb18cd8ff49f2ccafc6f8e6ed193
99embee <125384441+99embee@users.noreply.github.com>
2025-02-13 00:10:02 +0000

    updated code, moved old code to 'old' folder.

--- Character.py
+++ Character.py
@@ -1,35 +1,155 @@
+import json
+
 class Character:
-    # hp = 0
-    def __init__(self, name, race, char_class, level, race_hp, strength, dexterity, constitution, intelligence, wisdom, charisma):
-        self.name = name
-        self.race = race
-        self.char_class = char_class
-        self.level = level
-        self.hp = race_hp
-        self.strength = strength
-        self.dexterity = dexterity
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.wisdom = wisdom
-        self.charisma = charisma
+    def __init__(self, Role, Name, Level, Eperience, Race, Class, Health, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma):
+        self.Role = Role
+        self.Name = Name
+        self.Level = Level
+        self.Experience = Eperience
+        self.Race = Race
+        self.Class = Class
+        self.Health = Health
+        self.Strength = Strength
+        self.Dexterity = Dexterity
+        self.Constitution = Constitution
+        self.Intelligence = Intelligence
+        self.Wisdom = Wisdom
+        self.Charisma = Charisma
 
-    def level_up(self , hp):
-        self.level += 1
-        self.hp += hp
+    # def isJsonEmpty(Json):
+    #     try:
+    #         with open(Json, 'r', encoding='utf-8') as f:
+    #             data = json.load(f)
+    #             return len(data) == 0
+    #     except (FileNotFoundError, json.JSONDecodeError):
+    #         return True
+
+    def checkUnique(self):
+        try:
+            with open('Characters.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                # print('Data loaded from JSON:', data)
+                # for i, player in data:
+                for i in range(0, len(data)):
+                    # print('Player data:', data[i])
+                    # print('data---------- ', data[i]['Player']['Attributes']['Name'])
+                    if data[i]['Player']['Attributes']['Name'] == self.Name and data[i]['Player']['Attributes']['Race'] == self.Race and data[i]['Player']['Attributes']['Class'] == self.Class:
+                        return False
+                return True
+        except (FileNotFoundError, json.JSONDecodeError):
+            return True
+
+    def save(self):
+        try:
+            with open('Characters.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                self.id = data[-1][self.Role]['Attributes']['ID'] + 1
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = []
+            self.id = 1
+
+        player = {'Ref' : str(self.id) + '-'+self.Name, self.Role: 
+                    { 'Attributes':
+                        {'ID' : self.id, 'Name' : self.Name, 'Level' : self.Level, 'Experience': self.Experience, 'Race' : self.Race,
+                        'Class' : self.Class, 'Health' : self.Health, 
+                        'Abilities':{'Strength' : self.Strength,
+                                     'Dexterity' : self.Dexterity, 'Constitution' : self.Constitution, 
+                                     'Intelligence' : self.Intelligence, 'Wisdom' : self.Wisdom, 'Charisma' : self.Charisma}},
+                        'Extra' : {'Alignment' : '', 'Background' : '', 'Personality' : '', 'Ideals' : '', 'Bonds' : '', 'Flaws' : ''},
+                        'Details' : {'Feats' : [], 'Spells' : {'Cantrips' : {}, 
+                                                               '1st Level': {'1st Spell Slots': {}, '1st lvl Spells': {}},
+                                                               '2nd Level': {'2nd Spell Slots': {}, '2nd lvl Spells': {}}, 
+                                                               '3rd Level': {'3rd Spell Slots': {}, '3rd lvl Spells': {}}, 
+                                                               '4th Level': {'4th Spell Slots': {}, '4th lvl Spells': {}}, 
+                                                               '5th Level': {'5th Spell Slots': {}, '5th lvl Spells': {}}, 
+                                                               '6th Level': {'6th Spell Slots': {}, '6th lvl Spells': {}}, 
+                                                               '7th Level': {'7th Spell Slots': {}, '7th lvl Spells': {}}, 
+                                                               '8th Level': {'8th Spell Slots': {}, '8th lvl Spells': {}}, 
+                                                               '9th Level': {'9th Spell Slots': {}, '9th lvl Spells': {}}},
+                                      'Skills' : {'Skills' : []}, 'Languages' : {'Languages' : []}
+                        },
+                        'Inventory' : 
+                        {'Coins': 
+                            {'Platinum': 0, 'Gold' : 0, 'Silver': 0, 'Copper': 0},
+                        'Equipped Weapon': [],'Weapons' : [], 'Equipped Armour' : [], 'Armour' : [], 'Items' : [] }
+                    }
+                 }
+                  
         
+        data.append(player)
+        with open('Characters.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        
+    def search(self, ID):
+        with open('Characters.json') as f:
+            data = json.load(f)
+            for i in range(0, len(data)):
+                if data[i]['Player']['Attributes']['ID'] == ID:
+                    return True
+            else:
+                return False
 
-    def take_damage(self, damage):
-        self.hp -= damage
-        if self.hp < 0:
-            self.hp = 0
-
-    def heal(self, amount):
-        self.hp += amount
-        # Assuming max HP is 10 * level for simplicity
-        if self.hp > 10 * self.level:
-            self.hp = 10 * self.level
-
+    def load(Name, Race, Class):
+        with open('Characters.json') as f:
+            data = json.load(f)
+            for i in range(0, len(data)):
+                # print(data[i]['Player']['Attributes']['Name'])
+                # print(data[i]['Player']['Attributes']['Race'])
+                # print(data[i]['Player']['Attributes']['Class'])
+                if data[i]['Player']['Attributes']['Name'] == Name and data[i]['Player']['Attributes']['Race'] == Race and data[i]['Player']['Attributes']['Class'] == Class:
+                    player = data[i]['Player']['Attributes']
+                    # print(player)
+                    return player
+            else:
+                return False
+        
     def __str__(self):
-        return (f"Name: {self.name}, Race: {self.race}, Class: {self.char_class}, Level: {self.level}, HP: {self.hp}, "
-                f"STR: {self.strength}, DEX: {self.dexterity}, CON: {self.constitution}, INT: {self.intelligence}, "
-                f"WIS: {self.wisdom}, CHA: {self.charisma}")
+        return f'{self.Name} is a level {self.Level} {self.Race} {self.Class} with {self.Health} health, {self.Strength} strength, {self.Dexterity} dexterity, {self.Constitution} constitution, {self.Intelligence} intelligence, {self.Wisdom} wisdom, and {self.Charisma} charisma.'
+
+    feats = [{'Sentinel', "A successful OA reduce creature's speed to 0 for this turn and possibility to make an OA even if the ennemy take Disengage."},
+            {'Great Weapon Master', 'Extra attack after a melee critical hit and you can choose to take -5 to attack roll to add +10 to damage with an heavy weapon.'},
+            {'Sharpshooter', 'Extra attack after a ranged critical hit and you can choose to take -5 to attack roll to add +10 to damage with a ranged weapon.'},
+            {'Mage Slayer', 'Advantage on saving throw against spell and opportunity attack against a creature casting a spell.'},
+            {'Lucky', 'You can reroll 1, 2 or 3 on a d20 roll.'}]
+
+    def level_up(self, choice):
+        print('first line')
+        print(f'{self.Name} leveled up!')
+        self.Level += 1
+        if choice == 'increase ability scores':
+            print('You can increase one ability score by 2 or two ability scores by 1.')
+            cost = 2
+            while cost > 0:
+                ability = input('Which ability would you like to increase? ').lower()
+                try:
+                    increase = int(input('Would you like to increase that ability by 1 or 2? '))
+                    if increase not in [1, 2]:
+                            raise ValueError('Invalid increase amount')
+                except ValueError as e:
+                    print(e)
+                    continue
+
+                if ability == 'strength':
+                    self.Strength += increase
+                elif ability == 'dexterity' :
+                    self.Dexterity += increase
+                elif ability == 'constitution' :
+                    self.Constitution += increase
+                elif ability == 'intelligence':
+                    self.Intelligence += increase
+                elif ability == 'wisdom':
+                    self.Wisdom += increase
+                elif ability == 'charisma':
+                    self.Charisma += increase
+                else:
+                    print('Invalid ability. Please try again.')
+                    continue
+
+                cost -= increase
+
+        elif choice == 'feat':
+            
+            for title, desc in Character.feats:
+                print(f'{title}: {desc} \n')
+            feat = input('Which feat would you like to take? ')
+            self.feat.append(feat)
